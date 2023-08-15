c69157b99697d6a1250fea3f52cab3bd10ac1a4e
Liza Arora <liza.arora@outlook.com>
2023-08-16 00:11:16 +0800

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let message
+  if (ride <= 400){
+    message = 'This one is on me!'
+  } else if (ride > 400 && ride <2000){
+    message = 'That will be twenty bucks.'
+  } else if (ride > 2000 && ride < 2500){
+    message = 'I will gladly take your thirty bucks.'
+  } else if (ride > 2500){
+    message = 'No can do.'
+  }
+  return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let generous
+  switch (tip) {
+    case tip = 'generous':
+      return "Thank you so much."
+    case tip = 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
